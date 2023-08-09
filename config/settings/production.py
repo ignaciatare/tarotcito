1fffb80c603a695123320fd35113d22b92d8c8b9
Ignacia Taré Albornoz <101875879+ignaciatare@users.noreply.github.com>
2023-08-09 19:29:03 -0400

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -24,20 +24,16 @@ DATABASES = {
 
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa: F405
 
-# CACHES
-# ------------------------------------------------------------------------------
+
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env("REDIS_URL"),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            # Mimicing memcache behavior.
-            # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
-            "IGNORE_EXCEPTIONS": True,
-        },
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
     }
 }
+
+
+
 
 # SECURITY
 # ------------------------------------------------------------------------------
